a478f9c0a9f83e11413a8b258c52bba68e51cc0f
shivolen moodley <90754782+shivolen@users.noreply.github.com>
2025-02-18 19:48:34 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import {
-  SiVisualstudiocode,
-  SiSlack,
-  SiVercel,
-  
-} from "react-icons/si";
+import { SiVisualstudiocode, SiSlack, SiVercel } from "react-icons/si";
 
 function Toolstack() {
   return (
@@ -18,14 +13,8 @@ function Toolstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
-      <Col xs={4} md={2} className="tech-icons">
-        <SiJupyter />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiGooglecolab />
       </Col>
     </Row>
   );
 }
-
 export default Toolstack;
